7e17702b6c8e90aa63112381a582fef962564c97
Valensi <vallitriya@gmail.com>
2023-07-04 21:30:08 +0300

    add f addMovie

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+    
 const ad = document.querySelector('.promo__adv');
 const imgs = ad.querySelectorAll('img');
 const title = ad.querySelector('.promo__adv-title');
 const bg = document.querySelector('.promo__bg');
 const genre = bg.querySelector('.promo__genre');
 const movieList = document.querySelector('.promo__interactive-list');
+const addForm = document.querySelector('.add');
+const inputForm = addForm.querySelector('.adding__input');
+const inputCheck = addForm.querySelector('[type = "checkbox"]');
 
 const movieDB = {
     movies: [
@@ -17,24 +22,60 @@ const movieDB = {
     ]
 };
 
-title.remove();
+const deleteAdv = (...arg) => {
+    arg[0].remove();
 
-imgs.forEach(img => {
-    img.remove();
-});
+    arg[1].forEach(img => {
+        img.remove();
+    });
+}
+deleteAdv(title, imgs)
 
-genre.textContent = 'Драма';
+const makeChange = (...arg) => {
+    arg[0].textContent = 'Драма';
 
-bg.style.cssText = 'background: url("img/bg.jpg")';
+    arg[1].style.cssText = 'background: url("img/bg.jpg") center 0/cover no-repeat;';
+}
+makeChange(genre, bg)
 
-function showMovies(movies){
-    movieList.innerHTML = '';
-    movieDB.movies.sort();
-    movies.forEach((mov, i) => {
-        movieList.innerHTML += `  
-        <li class="promo__interactive-item">${i + 1} ${mov}
+
+const sortArray = (arr) => {
+    arr.sort();
+}
+sortArray(movieDB.movies);
+
+function showMovies(films, parent){
+    parent.innerHTML = '';
+    
+    films.forEach((film, i) => {
+        parent.innerHTML += `  
+        <li class="promo__interactive-item">${i + 1} ${film}
             <div class="delete"></div>
         </li>`;
     })
 } 
-showMovies(movieDB.movies)
+showMovies(movieDB.movies, movieList)
+
+
+function addMovie(){
+
+    addForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        
+        let newMovie = inputForm.value;
+        const favorite = inputCheck.checked;
+
+        if(newMovie){
+            if(newMovie.length > 21){
+                newMovie = `${newMovie.substring(0,22)}...`
+            }
+            movieDB.movies.push(newMovie);
+            sortArray(movieDB.movies);
+            showMovies(movieDB.movies, movieList);
+        }
+        e.target.reset();
+    })
+}
+addMovie(movieDB.movies);
+
+});
